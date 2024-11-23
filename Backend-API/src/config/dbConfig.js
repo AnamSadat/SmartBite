dd4a38b27e13adf435a894a0a0e7f8b068898f27
zynfinty <fajarihsana15@gmail.com>
2024-11-23 23:24:07 +0700

    move to TS

--- Backend-API/src/config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  dialect: 'mysql',
-  host: process.env.DB_HOST,
-  logging: false,
-});
-
-module.exports = db;
