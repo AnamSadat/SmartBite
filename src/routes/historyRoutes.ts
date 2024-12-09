f448d5caa5d3b0ed20e1efbe421d963f4a36b011
zynfinty <fajarihsana15@gmail.com>
2024-12-09 18:38:01 +0700

    update

--- src/routes/historyRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getHistory } from "../controllers/historyController";
-
-const authRoute = express.Router();
-
-authRoute.get("/history", getHistory);
-
-export default authRoute;
