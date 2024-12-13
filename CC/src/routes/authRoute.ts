571a7b8eefd3e4196937b77481ef6d242affe2a0
Fajar Ihsana <fajarihsana15@gmail.com>
2024-12-13 16:02:05 +0700

    Delete CC directory

--- CC/src/routes/authRoute.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, register } from "../controllers/auth";
-
-const authRoute = express.Router();
-
-authRoute.post("/auth/login", login);
-authRoute.post("/auth/register", register);
-
-export default authRoute;
